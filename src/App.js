20d4a35497454c241b20f13704182e6d1c33f33b
tykim7882 <nikaido244@naver.com>
2020-11-25 23:25:32 +0900

    component life cycle test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import Potato from './Potato';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      <Potato />
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("App class constructor");
+  }
+  state = {
+    count : 0
+  }
+  add = () =>{
+    this.setState(current => ({count : current.count + 1}));   // setState는 object, setState는 새로운 state를 받아야함.
+  };
+  minus = () => {
+    this.setState(current => ({count : current.count - 1})); // react는 setState가 호출되면 state를 refresh하고 render function을 호출함
+  };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("component just updated");
+  }
+  componentWillUnmount(){
+    console.log("Good bye");
+  }
+  render(){
+    console.log("I'm rendering");
+    return (
+      <div>
+          <h1>The number is {this.state.count}</h1>
+          <button onClick={this.add}>Add</button>
+          <button onClick={this.minus}>Minus</button>
+      </div>
+      );
+    }
 }
 
 export default App;
+
+
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Potato(){
-    return (
-        <h4>I like Potato!</h4>
-    );
-}
-
-export default Potato;
